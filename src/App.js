5dcf287be5d02e1ffd77c856775ba4ca8f7ba4d4
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-04 15:50:33 +0200

    react router basic info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import {Redirect, Route, Switch} from 'react-router-dom';
+
+// pages
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+
+// components
+import MainHeader from './components/MainHeader/MainHeader';
+
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <div>
+            <MainHeader />
+            <main>
+                <Switch>
+                <Route path="/" exact>
+                    <Redirect to="/welcome" />
+                </Route>
+                <Route path="/welcome" >
+                    <Welcome />    
+                </Route>     
+                <Route exact path="/products" >
+                    <Products />    
+                </Route>    
+                <Route path="/products/:productId" >
+                    <ProductDetail />    
+                </Route>
+                </Switch>        
+            </main>
+        </div>
+    );
 }
 
 export default App;
