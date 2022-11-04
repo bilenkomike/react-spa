546f49ff6eec120f79aa2b1f2aa4a14c80c4f8d2
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-04 15:53:20 +0200

    second initial commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-import {Redirect, Route, Switch} from 'react-router-dom';
-
-// pages
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-
-// components
-import MainHeader from './components/MainHeader/MainHeader';
-
-
 function App() {
-    return (
-        <div>
-            <MainHeader />
-            <main>
-                <Switch>
-                <Route path="/" exact>
-                    <Redirect to="/welcome" />
-                </Route>
-                <Route path="/welcome" >
-                    <Welcome />    
-                </Route>     
-                <Route exact path="/products" >
-                    <Products />    
-                </Route>    
-                <Route path="/products/:productId" >
-                    <ProductDetail />    
-                </Route>
-                </Switch>        
-            </main>
-        </div>
-    );
+  return (
+    <div>
+
+    </div>
+  );
 }
 
 export default App;
