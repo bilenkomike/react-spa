896e7fe06bcaa64bb50fdaad35102495ba34d67f
Mike Bilenko <80154023+bilenkomike@users.noreply.github.com>
2022-11-05 15:46:25 +0200

    final commit before updates

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,15 +1,51 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
+
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  
+  const params = useParams();
+  const {sendRequest, status,data: loadedComments} = useHttp(getAllComments);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
   
+  const { quoteId } = params;
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
+
+  const onAddedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+
+  if(status === 'pending') {
+     
+    comments =  <div className="centred"><LoadingSpinner /></div>;
+  }
+
+  if(status === 'completed' && (loadedComments && loadedComments.length > 0)) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if(status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments =  <p className="centered">No Comments were added yet</p>;
+  }
+
+  
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
@@ -18,8 +54,8 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm quoteId={params.quoteId} onAddedComment={onAddedCommentHandler} />}
+     {comments}
     </section>
   );
 };
